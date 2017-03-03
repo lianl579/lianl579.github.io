b5262077382e13f448a70157bac4215fcbc22b02
Leqi Liang <lianl579@newschool.edu>
2017-03-03 06:13:21 +0000

    notes

--- liquid/liquid.js
+++ liquid/liquid.js
@@ -9,18 +9,19 @@ navItems[3] = document.getElementById("item4");
 navItems[4] = document.getElementById("item5");
 navItems[5] = document.getElementById("item6");
 
-backgrounds[0] = "url(https://goo.gl/PL2igY)";
-backgrounds[1] = "url(https://goo.gl/31iUQA)";
+backgrounds[0] = "url(http://www.laurakirste.com/wp-content/uploads/2012/06/Wallpaper-Print.jpg)";
+backgrounds[1] = "url(http://www.laurakirste.com/wp-content/uploads/2012/06/Wallpaper-Print.jpg)";
 backgrounds[2] = "url(https://goo.gl/8lC2Df)";
 backgrounds[3] = "url(https://goo.gl/rJJ6fp)";
 backgrounds[4] = "url(https://goo.gl/rJJ6fp)";
 backgrounds[5] = "url(https://goo.gl/rJJ6fp)";
 
-navItems.forEach( function( item, i ) {
+navItems.forEach( function( item, 0 ) {
   item.addEventListener("mouseover", function() {
-    document.body.style.backgroundImage = backgrounds[i];
+    document.body.style.backgroundImage = backgrounds[0];
   });
 });
+
 
 <script> 
 jQuery('#questionmarkdiv').hover(function() 
@@ -30,5 +31,3 @@ jQuery('#backgroundimage').css("background-image", "url(change to this.jpg)");
 jQuery('#container').css("background-image", "url(http://thumbs.dreamstime.com/thumblarge_536/12838649102C1cO5.jpg)"); 
 });
 </script>
-
-<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a>
