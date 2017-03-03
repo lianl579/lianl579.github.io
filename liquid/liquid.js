a81c516d64fd8075dce0bd170d8453f1bd9205c4
Leqi Liang <lianl579@newschool.edu>
2017-03-03 17:28:25 -0500

    updated liquid project after session with Bryant

--- liquid/liquid.js
+++ liquid/liquid.js
@@ -1,7 +1,11 @@
+// define variables, init emty arrays for backgrounds and navItems
 var body = document.body;
 var navItems = [];
 var backgrounds = [];
 
+
+// Add nav Items Below. 
+// ** Change the text between the quotes to match the ID's in your navigation
 navItems[0] = document.getElementById("item1");
 navItems[1] = document.getElementById("item2");
 navItems[2] = document.getElementById("item3");
@@ -9,25 +13,31 @@ navItems[3] = document.getElementById("item4");
 navItems[4] = document.getElementById("item5");
 navItems[5] = document.getElementById("item6");
 
-backgrounds[0] = "url(http://www.laurakirste.com/wp-content/uploads/2012/06/Wallpaper-Print.jpg)";
-backgrounds[1] = "url(http://www.laurakirste.com/wp-content/uploads/2012/06/Wallpaper-Print.jpg)";
+
+
+// Set the Background Image URL's
+// ** Change the url between the parenthesis to match the url you want for your background image
+backgrounds[0] = "url(picture/french.jpg)";
+backgrounds[1] = "url(https://goo.gl/31iUQA)";
 backgrounds[2] = "url(https://goo.gl/8lC2Df)";
 backgrounds[3] = "url(https://goo.gl/rJJ6fp)";
-backgrounds[4] = "url(https://goo.gl/rJJ6fp)";
+backgrounds[4] = "url(https://goo.gl/8lC2Df)";
 backgrounds[5] = "url(https://goo.gl/rJJ6fp)";
 
-navItems.forEach( function( item, 0 ) {
+
+// add an event listener for each nav item
+navItems.forEach( function( item, i ) {
   item.addEventListener("mouseover", function() {
-    document.body.style.backgroundImage = backgrounds[0];
+    document.body.style.backgroundImage = backgrounds[i];
   });
 });
 
 
-<script> 
-jQuery('#questionmarkdiv').hover(function() 
-{ 
-jQuery('#backgroundimage').css("background-image", "url(change to this.jpg)"); 
-}).mouseleave(function(){
-jQuery('#container').css("background-image", "url(http://thumbs.dreamstime.com/thumblarge_536/12838649102C1cO5.jpg)"); 
-});
-</script>
+// <script> 
+// jQuery('#questionmarkdiv').hover(function() 
+// { 
+// jQuery('#backgroundimage').css("background-image", "url(change to this.jpg)"); 
+// }).mouseleave(function(){
+// jQuery('#container').css("background-image", "url(http://thumbs.dreamstime.com/thumblarge_536/12838649102C1cO5.jpg)"); 
+// });
+// </script>
