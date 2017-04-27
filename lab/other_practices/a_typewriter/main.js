c61ddddd51675ec9bbc7db1a6b49f568b41017b4
Leqi Liang <lianl579@newschool.edu>
2017-04-27 04:27:46 -0400

    note

--- lab/other_practices/a_typewriter/main.js
+++ lab/other_practices/a_typewriter/main.js
@@ -8,30 +8,78 @@ var letters = {
     KeyB: {
     	url: 'img/b.png',
     },
-    KeyC: {},
-    KeyD: {},
-    KeyE: {},
-    KeyF: {},
-    KeyG: {},
-    KeyH: {},
-    KeyI: {},
-    KeyJ: {},
-    KeyK: {},
-    KeyL: {},
-    KeyM: {},
-    KeyN: {},
-    KeyO: {},
-    KeyP: {},
-    KeyQ: {},
-    KeyR: {},
-    KeyS: {},
-    KeyT: {},
-    KeyU: {},
-    KeyV: {},
-    KeyW: {},
-    KeyX: {},
-    KeyY: {},
-    KeyZ: {}
+    KeyC: {
+    	url: 'img/c.png',
+    },
+    KeyD: {
+    	url: 'img/d.png',
+    },
+    KeyE: {
+    	url: 'img/e.png',
+    },
+    KeyF: {
+    	url: 'img/f.png',
+    },
+    KeyG: {
+    	url: 'img/g.png',
+    },
+    KeyH: {
+    	url: 'img/h.png',
+    },
+    KeyI: {
+    	url: 'img/i.png',
+    },
+    KeyJ: {
+    	url: 'img/j.png',
+    },
+    KeyK: {
+    	url: 'img/k.png',
+    },
+    KeyL: {
+    	url: 'img/l.png',
+    },
+    KeyM: {
+    	url: 'img/m.png',
+    },
+    KeyN: {
+    	url: 'img/n.png',
+    },
+    KeyO: {
+    	url: 'img/o.png',
+    },
+    KeyP: {
+    	url: 'img/p.png',
+    },
+    KeyQ: {
+    	url: 'img/q.png',
+    },
+    KeyR: {
+    	url: 'img/r.png',
+    },
+    KeyS: {
+    	url: 'img/s.png',
+    },
+    KeyT: {
+    	url: 'img/t.png',
+    },
+    KeyU: {
+    	url: 'img/u.png',
+    },
+    KeyV: {
+    	url: 'img/v.png',
+    },
+    KeyW: {
+    	url: 'img/w.png',
+    },
+    KeyX: {
+    	url: 'img/x.png',
+    },
+    KeyY: {
+    	url: 'img/y.png',
+    },
+    KeyZ: {
+    	url: 'img/z.png',
+    }
 };
 
 
@@ -50,61 +98,3 @@ document.addEventListener('keydown', function(event) {
 
 });
 
-// var img = document.createElement("img");     //创建一个img元素  
-//         img.src="q.png";   //给img元素的src属性赋值  
-//         //bigImg.width="320";  //320个像素 不用加px  
-//         var div = document.getElementById('div'); //获得dom对象  
-//         div.appendChild(img);      //为dom添加子元素img  
-
-// // var bgHue = 0;
-
-
-
-// for (key in letters) {
-//     // bgHue += Math.floor( 360 / (Object.keys(letters).length) );
-//     letters[key].bgColor = "hsl(" + bgHue + ", 100%, 50%)";
-// };
-
-// document.addEventListener('keydown', function(event){
-//     if (event.keyCode >= 65 && event.keyCode <= 90) {
-
-
-//         // var div = document.createElement('div');
-//         // div.classList.add('letter');
-//         var imgHTML="<img src="q.png">" ;
-
-//         // add capital class
-//         if (event.shiftKey == true) {
-//             div.classList.add('capital');
-//         };
-
-//         div.style.backgroundColor = letters[event.code].bgColor;
-//         container.appendChild(div);
-
-//        // document.getElementById("insert").innerHTML='<img src="new_banner.jpg" height="100" width="100" />';
-
-//         // eg: a, b, z
-//         // console.log("key: " + event.key);
-
-//         // eg: KeyA, KeyB, etc
-//         // console.log("code: " + event.code);
-
-//         // eg: 65, 68, etc
-//         // console.log("code: " + event.keyCode);
-
-//         // log the color
-//         // console.log("color: " + letters[event.code].bgColor);
-
-//         // change the background color
-//         // document.documentElement.style.backgroundColor = letters[event.code].bgColor;
-
-//         // add a letter to container
-//         // container.innerHTML += event.key;
-//     } else if (event.keyCode == 32) {
-//         var div = document.createElement('div');
-//         div.classList.add('space');
-//         container.appendChild(div);
-//     } else if (event.keyCode == 8) {
-//         container.removeChild( container.lastChild );
-//     }
-// });
